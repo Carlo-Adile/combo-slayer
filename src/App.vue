cf2c65f11383f4ce0954133025d624aa792f9661
Carlo-Adile <adile.carlo.a@gmailcom>
2024-04-05 12:08:13 +0200

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,61 @@
 <script>
 import GameMaster from './components/GameMaster.vue';
-import TopBanner from './components/TopBanner.vue'
+import TopBanner from './components/TopBanner.vue';
+
+const imgUrls = import.meta.glob('../assets/UI/*.png');
+const imgHourglassUrls = import.meta.glob('../src/assets/hourglass/*.png');
+const imgEnemyUrls = import.meta.glob('../assets/enemies/*.png');
+const imgCursorsUrls = import.meta.glob('../src/assets/cursor/*.png');
+const soundUrls = import.meta.glob('../src/assets/sound/*.mp3');
+const soundWavUrls = import.meta.glob('../src/assets/sound/*.wav');
 
 export default {
   name: "App",
   data() {
     return {
-      enemy: null
+      enemy: null,
+      imgUrls: {},
+      imgHourglassUrls: {},
+      imgEnemyUrls: {},
+      imgCursorsUrls: {},
+      soundUrls: {},
+      soundWavUrls: {}
     }
   },
   components: {
     GameMaster,
     TopBanner
-
+  },
+  mounted() {
+    this.loadResources();
   },
   methods: {
-
-  },
-
+    loadResources() {
+      this.loadImages(imgUrls, 'imgUrls');
+      this.loadImages(imgHourglassUrls, 'imgHourglassUrls');
+      this.loadImages(imgEnemyUrls, 'imgEnemyUrls');
+      this.loadImages(imgCursorsUrls, 'imgCursorsUrls');
+      this.loadSounds(soundUrls, 'soundUrls');
+      this.loadSounds(soundWavUrls, 'soundWavUrls');
+    },
+    loadImages(imageUrls, propertyName) {
+      Object.keys(imageUrls).forEach((key) => {
+        imageUrls[key]().then((module) => {
+          // Aggiungi la nuova proprietà direttamente all'oggetto
+          this[propertyName][key] = module.default;
+        });
+      });
+    },
+    loadSounds(soundUrls, propertyName) {
+      Object.keys(soundUrls).forEach((key) => {
+        soundUrls[key]().then((module) => {
+          // Aggiungi la nuova proprietà direttamente all'oggetto
+          this[propertyName][key] = module.default;
+        });
+      });
+    }
+  }
 }
-
 </script>
 
 <template>
